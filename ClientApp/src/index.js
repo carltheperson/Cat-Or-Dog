8fe6bdd46da2ba10a76f92ec31c47da4ad52cf50
Carl Riis <carl.aagot.riis@gmail.com>
2020-11-06 17:35:58 +0100

    Removed unnecessary boilerplate

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -1,9 +1,7 @@
-import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
@@ -13,6 +11,3 @@ ReactDOM.render(
     <App />
   </BrowserRouter>,
   rootElement);
-
-registerServiceWorker();
-
---
+++ Controllers/API.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cat_Or_Dog.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class API : ControllerBase
+    {
+        [HttpGet]
+        public string Get() {
+            return "Hello world";
+        }
+    }
+}
