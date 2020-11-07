8dc18a57f594d3057828a3fdcbc20600fcb77823
Carl Riis <carl.aagot.riis@gmail.com>
2020-11-07 11:48:51 +0100

    React frontend

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import { Route } from 'react-router';
+import React, { useState } from 'react';
 import { MainPage } from './components/MainPage';
 
 export default () => {
-      return ( <div>
-        <Route exact path='/' component={MainPage} />
-      </div>
+  const [map, setMap] = useState("");
+  const [ready, setReady] = useState(false);
+
+      return ( 
+        <div>
+          <h1>🐈 Cat or Dog 🐕</h1>
+        {ready
+          ? <MainPage map={map} />
+          : <div>
+              <p>Type a map</p>
+              <input onChange={(e) => setMap(e.target.value)} value={map} />
+              <button onClick={() => setReady(true)}>Ready</button>
+            </div>
+        }
+        </div>
       )
 }
--- ClientApp/src/components/MainPage.js
+++ ClientApp/src/components/MainPage.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-export const MainPage = () => {
+export const MainPage = ({map}) => {
+  const [score, setScore] = useState(0);
+  const [peakScore, setPeakScore] = useState(1);
+  const [mode, setMode] = useState("Traning");
+  const [peaks, setPeaks] = useState([]);
+  const [winning, setWinning] = useState(true);
+
+  const toggleMode = () => {
+    if (mode === "Traning") setMode("Playing"); setWinning(true)
+    if (mode === "Playing") setMode("Traning"); setScore(0)
+   }
+
+   const getDecision = async (score) => {
+     return (await fetch(`api?seed=${map}&score=${score}`)).text();
+   }
+
+   const addNewPeak = async () => {
+      setPeaks(peaks.concat([(peakScore + " : " + await getDecision(peakScore))]));
+      setPeakScore(peakScore + 1);
+   }
+
+   const checkGuess = async (guess) => {
+     if ((await getDecision(score+1)) === guess) {
+       setScore(score + 1);
+     } else {
+        setWinning(false)
+      }
+
+     }
+
   return ( 
       <div>
-        <h1>Hello, world!</h1>
+        <p>Map: {map}</p>
+        <p>Mode: {mode}</p>
+        <button onClick={() => toggleMode()}>Toggle mode</button>
+        <p>-----------------------------</p>
+        {(mode === "Traning")
+          ? <div>
+              {peaks.map((peak, i) => {return <div key={i}>{peak}</div>})}
+              <button onClick={() => addNewPeak()}>New peak</button>
+          </div>
+          : <div><h2>Score: {score}</h2>
+            { winning
+              ? <div> 
+                  <button onClick={() => checkGuess("Cat")}>Cat</button>
+                  <button onClick={() => checkGuess("Dog")}>Dog</button>
+                 </div>
+              : <h3> YOU LOST</h3>
+          }</div>
+        }
       </div>
   )
 }
